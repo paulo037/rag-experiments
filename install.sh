677c0693e76ed93f5558d475de95461ae9eb194b
paulo037 <paulo.h.carneiro@ufv.br>
2025-03-31 02:19:29 -0300

    Update project structure with pyproject.toml and remove installation scripts

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install the package in development mode
-pip install -e .
-
-# Print success message
-echo "Installation complete!"
-echo "You can now run the demo with: python3 demo.py"
-echo "Or run the tests with: python3 -m unittest discover tests" 
